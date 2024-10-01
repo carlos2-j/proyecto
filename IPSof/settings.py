df1673e9c10ef4e56323b4ba19c7503aa58b345c
carlos2-j <adsicienaga@gmail.com>
2024-10-01 07:33:00 -0500

    cambio hora

--- IPSof/settings.py
+++ IPSof/settings.py
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'es-es'
+LANGUAGE_CODE = 'es-co'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/bogota'
 
 USE_I18N = True
 
